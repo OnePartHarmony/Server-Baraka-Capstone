d1db6fdb337b568eaefa6be25412ac60eb59a4cc
OnePartHarmony <bakingharmony@gmail.com>
2022-11-27 12:03:26 -0500

    removed comments in user function that were accidentally left in from copying and pasting route for reference

---
+++ app/routes/game_function_routes.js
@@ -0,0 +1,5 @@
+const Game = require('../models/game')
+const Territory = require('../models/territory')
+const Unit = require('../models/unit')
+const Player = require('../models/player')
+
--- app/routes/user_function_routes.js
+++ app/routes/user_function_routes.js
@@ -1,14 +1,12 @@
 const User = require('../models/user')
 
+//patch user model with socket room id
 const joinRoom = (currentUser, roomId) => {
-	// `req.user` will be determined by decoding the token payload
 	User.findById(currentUser._id)
-		// save user outside the promise chain
 		.then(user => {
 			user.gameRoomId = roomId
 			return user.save()
 		})
-		// pass any errors along to the error handler
 		.catch(err => console.log(err))
 }
 
