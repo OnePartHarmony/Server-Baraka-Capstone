c11b5a58b3bce8d848d1e8886762fd4f3c0115cb
OnePartHarmony <bakingharmony@gmail.com>
2022-11-27 16:42:49 -0500

    figured out making functions async in order to add to callback from multiple function calls

--- app/routes/user_function_routes.js
+++ app/routes/user_function_routes.js
@@ -1,14 +1,14 @@
 const User = require('../models/user')
 
 //patch user model with socket room id, return new user and roomId to client
-const joinRoom = (currentUser, roomId, callback) => {
-	User.findById(currentUser._id)
+async function joinRoom (currentUser, roomId, addToCallback) {
+	await User.findById(currentUser._id)
 		.then(user => {
 			user.gameRoomId = roomId
 			return user.save()
 		})
 		.then(user => {
-			callback({ roomId: roomId, user: user.toObject() })
+			addToCallback( {roomId: roomId, user: user.toObject() } )
 		})
 		.catch(err => console.log(err))
 }
