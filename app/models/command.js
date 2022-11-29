2495dd36bf9d84b801d067e47aeb550aa47de224
zyaffee <zyaffee@gmail.com>
2022-11-29 16:49:46 -0500

    made combat nice and separate, commands return true if they worked, false if they didn't, and 'combat' if combat

--- app/models/command.js
+++ app/models/command.js
@@ -75,81 +75,82 @@ commandSchema.methods.executeCommand = function executeCommand() {
 	if (this.issuedBy !== origin.controlledBy) {
 
 		// command was cancelled, notify player
-		return
+		return false
 	}
 
 	switch (this.type) {
 		case 'advance':
 			// detectCombat will move units in or resolve combat then move units in
 			if (this.detectCombat()) {
-				let originTerrFormation = PROMISEGetThisFormationPROMISE()
-				let newTerrFormation = PROMISEGetThisFormationPROMISE()
+				// let originTerrFormation = PROMISEGetThisFormationPROMISE()
+				// let newTerrFormation = PROMISEGetThisFormationPROMISE()
 
-				if (this.combat(origin, originTerrFormation, target, newTerrFormation)){
-					unitsMarchIn()
-				}
+				// if (this.combat(origin, originTerrFormation, target, newTerrFormation)){
+				// 	unitsMarchIn()
+				// }
+				return "combat"
+
 			} else {
 				unitsMarchIn()
-                updateDocs()
+				updateDocs()
+                return true
 			}
-			break
 		case 'excise':
 			// this command cannot be a valid option in front end if territory wealth < 1 or no priests, this is backend double check
 			if (origin.wealth < 1 || !origin.priests) {
 				console.log("you can't tax without wealth nor without tax collectors")
-				break
+				return false
 			} else {
 				commander.gold += origin.wealth
 				origin.wealth -= 1
                 updateDocs()
-				break
+				return true
 			}
 		case 'muster':
 			// this command cannot be a valid option in front end if territory population < 1 or no priests, this is backend double check
 			if (origin.population < 1 || !origin.priests) {
 				console.log("you can't recruit without recruits nor without recruiters")
-				break
+				return false
 			} else {
 				// create new unit and add to origin's units
 				if (this.musteredUnit === 'soldier') {
 					// back end check if you can afford this
 					if (commander.gold < 2) {
 						console.log('not enough gold')
-						break
+						return false
 					} else {
 						origin.soldiers += 1
 						commander.gold -= 2
-						origin.abundance -= 1
-                        updateDocs()
+						origin.abundance -= 1                        
 					}
 				} else if (this.musteredUnit === 'priest') {
 					// back end check if you can afford this
 					if (commander.gold < 5) {
 						console.log('not enough gold')
-						break
+						return false
 					} else {
 						origin.priests += 1
 						commander.gold -= 5
 						origin.abundance -= 1
-                        updateDocs()
 					}
 				}
 				// assuming peasants, rather than being full units in their own right, are just numeric representations of population
 				origin.population -= 1
-				break
+				updateDocs()
+				return true
 			}
 		case 'sow':
 			// this command cannot be a valid option in front end if population < 1, this is backend double check
 			if (origin.population < 1) {
 				console.log('you need peasants to sow')
-				break
+				return false
 			} else {
 				origin.population += 1
 				origin.abundance += 2
                 updateDocs()
 			}
 		default:
-			break
+			return false
 	}
 }
 
@@ -168,7 +169,35 @@ commandSchema.methods.detectCombat = function detectCombat() {
 }
 
 // potential combat function
-commandSchema.methods.combat = function combat(origin, originTerrFormation, target, newTerrFormation) {
+commandSchema.methods.combat = function combat(originTerrFormation, newTerrFormation) {
+
+	let commander = Player.findById(this.issuedBy.id)
+
+	let origin = Territory.findById(this.originTerritory.id)
+
+	let target
+
+	if (this.newTerritory) {
+		target = Territory.findById(this.newTerritory.id)
+	}
+
+    // saves all the pulled in documents
+    const updateDocs = function updateDocs() {
+        origin.save()
+        commander.save()
+        if (target) {
+            target.save()
+        }
+    }
+
+	// for moving marching units from one territory into another
+	const unitsMarchIn = function unitsMarchIn() {
+		origin.soldiers -= soldiersMarching
+		target.soldiers += soldiersMarching
+		origin.priests -= priestsMarching
+		target.priests += priestsMarching
+		target.controlledBy = origin.controlledBy
+	}
 
 	// grab initial attack strength
 	let attackStrength = this.priestsMarching + (this.soldiersMarching * 2)
@@ -179,45 +208,48 @@ commandSchema.methods.combat = function combat(origin, originTerrFormation, targ
 	}
 
 	// initial defense strength
-	let defenseStrength = target.priests + (2 * target.soldiers)
+	let defenseStrength = this.newTerritory.priests + (2 * this.newTerritory.soldiers)
 
 	// bonus for leadership
-	if (target.priests) {
+	if (this.newTerritory.priests) {
 		defenseStrength += target.soldiers
 	}
 
 	// mountain terrain buff
-	if (origin.type === 'mountain' && target.type !== 'mountain') {
+	if (origin.type === 'mountain' && this.newTerritory.type !== 'mountain') {
 		attackStrength += 3
-	} else if (target.type === 'mountain' && origin.type !== 'mountain') {
+	} else if (this.newTerritory.type === 'mountain' && origin.type !== 'mountain') {
 		defenseStrength += 3
 	}
 
 	// formation bonus, most significant by far so perhaps should come before some other bonuses
-	let originRoll = dice.roll(originTerrFormation)
-	let targetRoll = dice.roll(newTerrFormation)
+	let attRoll = dice.roll(originTerrFormation)
+	let defRoll = dice.roll(newTerrFormation)
 
-	if (originRoll > targetRoll) {
+	if (attRoll > defRoll) {
 		attackStrength *= 2
 	} else {
 		defenseStrength *= 2
 	}
-
-	PROMISESendResultsToFrontEndPROMISE(originRoll, targetRoll, attackStrength, defenseStrength)
 
 	// result
 	if (attackStrength > defenseStrength) {
 		// destroy all defenders
 		target.priests = 0
 		target.soldiers = 0
-
-		return true
+		unitsMarchIn()
 	} else {
 		// destroy all attackers
 		origin.priests -= this.priestsMarching
 		origin.soldiers -= this.soldiersMarching
+	}
+	updateDocs()
 
-		return false
+	return {
+		attRoll: attRoll,
+		defRoll: defRoll,
+		attackFinal: attackStrength,
+		defenseFinal: defenseStrength
 	}
 }
 
